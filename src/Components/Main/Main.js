3f773d38a3f51840f3ba9d265b403360cf0e17bb
tonyg <aj.gianno@gmail.com>
2021-02-08 19:15:01 -0600

    stuff

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Main.css';
 
 export default function Main() {
-  return <div>Hello, Portfolio!</div>;
+  return <div classname='main'>Hello, Portfolio!</div>;
 }
