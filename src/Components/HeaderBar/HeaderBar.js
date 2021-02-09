0ca1ae38edae64b99fcc32ef0b62f88308dd7306
tonyg <aj.gianno@gmail.com>
2021-02-08 22:11:15 -0600

    yolo

--- src/Components/HeaderBar/HeaderBar.js
+++ src/Components/HeaderBar/HeaderBar.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import '/HeaderBar.css';
+import './HeaderBar.css';
+import { Link } from 'react-router-dom';
 
+import face from '../../Images/soho-self.jpg';
+import launch from '../../Images/launch.png';
+import github from '../../Images/github.png';
+import linkedin from '../../Images/linkedin.png';
 
 export default function HeaderBar() {
   return (
-    
-  )
+    <div className='header-bar'>
+      <img className='face-img' src={face} alt='face-img' />
+      <p>
+        <Link
+          to='httds://github.com/agiannotti'
+          target='_blank'
+          style={{ textDecoration: 'none' }}
+        >
+          <img className='git-img' src={github} alt='github' />
+          GitHub
+          <img className='launch-img' src={launch} alt='launch-img' />
+        </Link>
+      </p>
+      <p>
+        <Link
+          to='https://www.linkedin.com/in/agiannotti/'
+          target='_blank'
+          style={{ textDecoration: 'none' }}
+        >
+          <img className='linkedin-img' src={linkedin} alt='linkedin' />
+          LinkedIn
+          <img className='launch-img' src={launch} alt='launch-img' />
+        </Link>
+      </p>
+    </div>
+  );
 }
