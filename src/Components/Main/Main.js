68c6d0ae17701002dbb7cc626a5c765638b0371a
tonyg <aj.gianno@gmail.com>
2021-02-10 11:55:23 -0600

    bio

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -10,10 +10,10 @@ export default function Main() {
         <header className='header'>
           <h1>Hello, I'm Anthony Giannotti.</h1>
           <p>
-            I'm a Chicago-based programmer, primarily focusing on Javascript,
-            React, SQL, and Node. I have a passion for dynamic applications,
-            thinking broadly, and tuning into the latest advancements in
-            software development.
+            I'm a Chicago-based software developer, primarily focusing on
+            Javascript, React, SQL, and Node. I have a passion for dynamic
+            applications, thinking broadly, and tuning into the latest
+            advancements in web technologies.
           </p>
         </header>
         <HeaderBar />
