7faf087bb7bc450e2d09c0d719e8145bd938a8dc
tonyg <aj.gianno@gmail.com>
2021-04-07 22:16:39 -0500

    flexbox nth children

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,43 +10,49 @@ import emailimg from '../../Images/Thumbnails/Email.png';
 export default function HeaderBar() {
   return (
     <div className='header-box'>
-      <img className='face-img' src={face} alt='face-img' />
-      <div className='header-links'>
-        <a
-          target={'_blank'}
-          rel='noopener noreferrer'
-          href='https://github.com/agiannotti'
-          style={{ textDecoration: 'none' }}
-        >
-          <img className='git-img' src={github} alt='github' />
-          GitHub
-          <img className='launch-img' src={launch} alt='launch-img' />
-        </a>
-      </div>
-      <div className='header-links'>
-        <a
-          target={'_blank'}
-          rel='noopener noreferrer'
-          href='https://www.linkedin.com/in/agiannotti/'
-          style={{ textDecoration: 'none' }}
-        >
-          <img className='linkedin-img' src={linkedin} alt='linkedin' />
-          LinkedIn
-          <img className='launch-img' src={launch} alt='launch-img' />
-        </a>
-      </div>
-      <div className='header-links'>
-        <a
-          target={'_blank'}
-          rel='noopener noreferrer'
-          href={`mailto:${'aj.gianno@gmail.com'}`}
-          style={{ textDecoration: 'none' }}
-        >
-          <img className='email-img' src={emailimg} alt='email' />
-          Email
-          <img className='launch-img' src={launch} alt='launch-img' />
-        </a>
-      </div>
+      <li>
+        <div>
+          <span>
+            <img id='face-img' src={face} alt='face-img' />
+          </span>
+        </div>
+        <div>
+          <a
+            target={'_blank'}
+            rel='noopener noreferrer'
+            href='https://github.com/agiannotti'
+            style={{ textDecoration: 'none' }}
+          >
+            <img className='git-img' src={github} alt='github' />
+            GitHub
+            <img className='launch-img' src={launch} alt='launch-img' />
+          </a>
+        </div>
+        <div>
+          <a
+            target={'_blank'}
+            rel='noopener noreferrer'
+            href='https://www.linkedin.com/in/agiannotti/'
+            style={{ textDecoration: 'none' }}
+          >
+            <img className='linkedin-img' src={linkedin} alt='linkedin' />
+            LinkedIn
+            <img className='launch-img' src={launch} alt='launch-img' />
+          </a>
+        </div>
+        <div>
+          <a
+            target={'_blank'}
+            rel='noopener noreferrer'
+            href={`mailto:${'aj.gianno@gmail.com'}`}
+            style={{ textDecoration: 'none' }}
+          >
+            <img className='email-img' src={emailimg} alt='email' />
+            Email
+            <img className='launch-img' src={launch} alt='launch-img' />
+          </a>
+        </div>
+      </li>
     </div>
   );
 }
